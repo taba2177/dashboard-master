b679354ca2758bb762b126d2d8c4cb0da9b8e314
taba2177 <tabadev217@gmail.com>
2024-08-12 14:13:43 +0300

    #

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://website-b93f2faa.pkh.sga.mybluehost.me'),
 
     'asset_url' => env('ASSET_URL', "//"),
 
