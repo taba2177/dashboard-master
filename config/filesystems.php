4b94730f195c88755c3b1ee9df8adc957398ae43
taba2177 <tabadev217@gmail.com>
2024-08-18 21:10:21 +0300

    #

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,11 +44,11 @@ return [
             'permissions' => [
                 'file' => [
                     'public' => 0644,
-                    'private' => 0755,
+                    'private' => 0600,
                 ],
                 'dir' => [
-                    'public' => 0755,  // You can change this to 0775 if needed
-                    'private' => 0755,
+                    'public' => 0775,  // You can change this to 0775 if needed
+                    'private' => 0700,
                 ],
             ],
         ],
