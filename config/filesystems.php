4844bc78d9c62d67c81fc1b47fd270940e9885fc
taba2177 <tabadev217@gmail.com>
2024-08-19 20:33:46 +0300

    #

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,12 +51,21 @@ return [
                 ],
             ],
         ],
-        'media' => [
-            'driver' => 'local',
-            'root'   => public_path('app'),
-            'url'    => env('APP_URL').'/storage',
-            'visibility' => 'public',
-        ],
+'media' => [
+'driver' => 'local',
+'root' => public_path('media'),
+'url' => env('APP_URL').'/media',
+'permissions' => [
+'file' => [
+'public' => 0774,
+'private' => 0600,
+],
+'dir' => [
+'public' => 0775,
+'private' => 0700,
+],
+],
+],
 
         's3' => [
             'driver' => 's3',
